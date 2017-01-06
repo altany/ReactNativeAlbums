5964be77fc605ca34d1ce71d01ab73ae2881da09
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2017-01-06 17:24:10 +0000

    unsupported styling

--- src/components/header.js
+++ src/components/header.js
@@ -20,7 +20,7 @@ const styles = {
     height: 60,
     paddingTop: 15,
     shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2},
+    shadowOffset: { width: 0, height: 2}, //ios only though - needs work
     shadowOpacity: 0.2,
     elevation: 2,
     position: 'relative'
