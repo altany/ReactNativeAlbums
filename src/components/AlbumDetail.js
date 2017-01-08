e40bc388c51160f26642fa5a297c31ca6f1b14af
Tania <taniapapazaf@gmail.com>
2017-01-08 19:49:42 +0000

    align the thumbnail to centre

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,11 +5,14 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
   const { title, artist, thumbnail_image } = album;
-  const { thumbnailStyle, headerContentStyle } = styles;
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    thumbnailContainerStyle } = styles;
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image
             style={thumbnailStyle}
             source={{ uri: thumbnail_image }}
@@ -32,6 +35,12 @@ const styles = {
   thumbnailStyle: {
     height: 50,
     width: 50
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
   }
 
 };
