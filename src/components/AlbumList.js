7de31f553aa7d5a5d499e737e0c5e83c25e69eda
Tania <taniapapazaf@gmail.com>
2017-01-08 11:51:46 +0000

    set and update the componet state with request result, render the result in the page

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,18 +2,23 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
   componentWillMount() {
     fetch('https://rallycoding.herokuapp.com/api/music_albums')
       .then(response => response.json())
-      .then(data => console.log(data));
+      .then(data => this.setState({ albums: data }));
+  }
+
+  renderAlbums() {
+      return this.state.albums.map(album => <Text>{album.title}</Text>);
   }
 
   render() {
+    console.log(this.state);
     return (
       <View>
-        <Text>
-          Album List
-        </Text>
+        {this.renderAlbums()}
       </View>
     );
   }
