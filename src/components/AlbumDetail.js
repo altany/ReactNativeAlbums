765e32520ff93e7839ea6d8cd5349eba64125cab
Tania <taniapapazaf@gmail.com>
2017-01-20 11:16:22 +0000

    add Linking to make button functional

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
@@ -9,7 +9,9 @@ const AlbumDetail = ({ album }) => {
     title,
     artist,
     thumbnail_image,
-    image } = album;
+    image,
+    url
+  } = album;
   const {
     thumbnailStyle,
     headerContentStyle,
@@ -37,7 +39,9 @@ const AlbumDetail = ({ album }) => {
       </CardSection>
 
       <CardSection>
-        <Button />
+        <Button onPress={() => Linking.openURL(url)} >
+          Buy Now
+        </Button>
       </CardSection>
     </Card>
   );
